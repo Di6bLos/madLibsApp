ba5ad8b2d3c33e42b609428aea2c002f0532ae1f
CarlosPrieto <87465747+Di6bLos@users.noreply.github.com>
2022-07-03 12:30:32 -0700

    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 let userInputs = [];
 
 let originalStory = 
-`Deep in the jungle, ${userInputs[0]} and I suddenly heard ${userInputs[0]} ${userInputs[0]}.
-'Watch out!' shouted ${userInputs[0]}.'That ${userInputs[0]} ${userInputs[0]} is going to get you!'
-It had very ${userInputs[0]} ${userInputs[0]} and a huge ${userInputs[0]}, so we ${userInputs[0]} onto our ${userInputs[0]} and ${userInputs[0]},
-${userInputs[0]} away from the dark, ${userInputs[0]} forest.`;
+`Deep in the jungle, ${userInputs[0]} and I suddenly heard ${userInputs[1]} ${userInputs[2]}.
+'Watch out!' shouted ${userInputs[0]}.'That ${userInputs[3]} ${userInputs[4]} is going to get you!'
+It had very ${userInputs[5]} ${userInputs[6]} and a huge ${userInputs[7]}, 
+so we ${userInputs[8]} onto our ${userInputs[9]} and ${userInputs[10]} away from the dark, ${userInputs[11]} forest.`;
 
-
+let numberOfQuestions = 12;
 
 
 console.log(originalStory);
@@ -23,8 +23,7 @@ console.log(originalStory);
 /* orginalStory - 
 
 Deep in the jungle, Jay and I suddenly heard loud bellows.
-'Carlos, Watch out!' shouted Jay.'That green monkey is going to get you!'
-It had very sharp claws and a huge mouth, so we jumped onto our horses and ran,
-rapidly away from the dark, dense forest.
+'Watch out!' shouted Jay.'That green monkey is going to get you!'
+It had very sharp claws and a huge mouth, so we jumped onto our horses and ran away from the dark, dense forest.
 
 */
