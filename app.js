8ffc5cf7d3f93e125b1044ae54eb355b20709e20
CarlosPrieto <87465747+Di6bLos@users.noreply.github.com>
2022-07-03 13:42:15 -0700

    Added questionCounter and for loop

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 "use strict";
 
+
+// User Prompts
+
+let intro = '- Please enter a ' 
+
+let questionArray = 
+['name of a friend.',
+'nasty adjective.',
+'animal sound.',
+'color.',
+'animal.',
+'adjective.',
+'body parts (plural).',
+'body part.',
+'verb (past-tense).',
+'animal (plural).',
+'verb (past-tense).',
+'adjective.'];
+
+let questionCounter = 12;
+
+
+
+for (let questions of questionArray){
+    console.log(questionCounter-- + intro + questions);
+    console.log
+}
+
+
 // The Story
 
 let userInputs = [];
 
+
 let originalStory = 
-`Deep in the jungle, ${userInputs[0]} and I suddenly heard ${userInputs[1]} ${userInputs[2]}.
+`Deep in the jungle, ${userInputs[0]} and I suddenly heard ${userInputs[1]} ${userInputs[2]}s.
 'Watch out!' shouted ${userInputs[0]}.'That ${userInputs[3]} ${userInputs[4]} is going to get you!'
 It had very ${userInputs[5]} ${userInputs[6]} and a huge ${userInputs[7]}, 
 so we ${userInputs[8]} onto our ${userInputs[9]} and ${userInputs[10]} away from the dark, ${userInputs[11]} forest.`;
@@ -13,29 +43,25 @@ so we ${userInputs[8]} onto our ${userInputs[9]} and ${userInputs[10]} away from
 let numberOfQuestions = 12;
 
 
-// User Prompts
-
-let questionArray = 
-['Enter name of best friend.',
-'Enter nasty adjective.',
-'Enter animal sound.',
-'Enter color.',
-'Enter animal.',
-'Enter adjective.',
-'Enter body parts (plural).',
-'Enter body part.',
-'Enter verb (past-tense).',
-'Enter animal (plural).',
-'Enter verb (past-tense).',
-'Enter adjective.'];
-
-
-
-
 /* orginalStory - 
 
 Deep in the jungle, Jay and I suddenly heard loud bellows.
 'Watch out!' shouted Jay.'That green monkey is going to get you!'
 It had very sharp claws and a huge mouth, so we jumped onto our horses and ran away from the dark, dense forest.
+
+let userInputs = [
+    'Jay',
+    'loud',
+    'bellow',
+    'green',
+    'monkey',
+    'sharp',
+    'claws',
+    'mouth',
+    'jumped',
+    'horses',
+    'ran',
+    'dense'
+];
 
 */
