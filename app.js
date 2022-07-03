62119ba2b3c4da27301298ca7c6dc4ce194b1209
CarlosPrieto <87465747+Di6bLos@users.noreply.github.com>
2022-07-03 12:18:35 -0700

    added madlib replacement variables

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+let userInputs = [];
+
+let originalStory = 
+`Deep in the jungle, ${userInputs[0]} and I suddenly heard ${userInputs[0]} ${userInputs[0]}.
+'Watch out!' shouted ${userInputs[0]}.'That ${userInputs[0]} ${userInputs[0]} is going to get you!'
+It had very ${userInputs[0]} ${userInputs[0]} and a huge ${userInputs[0]}, so we ${userInputs[0]} onto our ${userInputs[0]} and ${userInputs[0]},
+${userInputs[0]} away from the dark, ${userInputs[0]} forest.`;
+
+
+
+
+console.log(originalStory);
+
+
+
+
+
+
+
+/* orginalStory - 
+
+Deep in the jungle, Jay and I suddenly heard loud bellows.
+'Carlos, Watch out!' shouted Jay.'That green monkey is going to get you!'
+It had very sharp claws and a huge mouth, so we jumped onto our horses and ran,
+rapidly away from the dark, dense forest.
+
+*/
