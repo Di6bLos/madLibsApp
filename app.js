fbd8f2d46c38fe34345944bd48f8981d15569f6b
CarlosPrieto <87465747+Di6bLos@users.noreply.github.com>
2022-07-03 15:03:05 -0700

    userInputs added to story, prompts complete

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ let questionTotal = 12;
 
 let questionCounter = 0;
 
-let userInputs = [];
-
 let intro = 'Please enter a ' ;
 
 let questionArray = [
@@ -26,11 +24,22 @@ let questionArray = [
 'adjective.'
 ];
 
-for (let i = questionTotal; i >= 0; i--){
-    console.log(i);
+let userInputs = [];
 
+for (let i = questionTotal; i > 0; i--){
+
+    console.log(intro+questionArray[questionCounter] + `(${questionTotal} questions left)`); 
+
+    userInputs.push(prompt(intro+questionArray[questionCounter] + ` (${questionTotal} questions left)`));
+
+
+    questionCounter++;
+    questionTotal--;    
 }
 
+console.log(userInputs);
+
+alert('All done. Check out your story!')
 
 
 // The Story
@@ -38,12 +47,13 @@ for (let i = questionTotal; i >= 0; i--){
 
 
 let originalStory = 
-`Deep in the jungle, ${userInputs[0]} and I suddenly heard ${userInputs[1]} ${userInputs[2]}s.
+`<h3>Deep in the jungle, ${userInputs[0]} and I suddenly heard ${userInputs[1]} ${userInputs[2]}s.
 'Watch out!' shouted ${userInputs[0]}.'That ${userInputs[3]} ${userInputs[4]} is going to get you!'
 It had very ${userInputs[5]} ${userInputs[6]} and a huge ${userInputs[7]}, 
-so we ${userInputs[8]} onto our ${userInputs[9]} and ${userInputs[10]} away from the dark, ${userInputs[11]} forest.`;
+so we ${userInputs[8]} onto our ${userInputs[9]} and ${userInputs[10]} away from the dark, ${userInputs[11]} forest.</h3>`;
 
-
+console.log(originalStory);
+document.write(originalStory);
 
 /* orginalStory - 
 
